8f469a8459906ccd58e86d9750210c5120fd8102
Gabriel Torres <gaabrieltt7@gmail.com>
2023-08-06 15:10:11 -0300

    feat: add csvFile model on db

---
+++ prisma/migrations/20230806180311_model_csv_file/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "csv_files" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "favorite_sport" TEXT NOT NULL
+);
