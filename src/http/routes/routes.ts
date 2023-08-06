2963363ce79099cb1ce34d1bfcbce61440c4f52c
Gabriel Torres <gaabrieltt7@gmail.com>
2023-08-06 20:46:09 -0300

    feat: search route

--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import multerConfig from '../../utils/multer';
 import { csvFileUpload } from '../../http/controllers/csvFileUpload';
+import { search } from '../controllers/search';
 
 const router = Router();
 
 router.post('/api/files', multerConfig.single('file'), csvFileUpload);
+router.get('/api/users', search);
 
 
 export { router }
