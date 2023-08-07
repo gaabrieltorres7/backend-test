5607a8a4e1e03ade2e64e878b38f111dcb323af1
Gabriel Torres <gaabrieltt7@gmail.com>
2023-08-07 13:28:04 -0300

    refactor: add try catch

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,46 +1,80 @@
-import prisma from '../../db/client'
-import { Prisma } from '@prisma/client'
-import { UsersRepository } from '../users-repository'
+import prisma from "../../db/client";
+import { Prisma } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
 
-export class PrismaUsersRepository implements UsersRepository{
-  async insert({ name, city, country, favorite_sport }: Prisma.UserCreateInput) {
-    const user = await prisma.user.create({
-      data: {
-        name,
-        city,
-        country,
-        favorite_sport
-      },
-    })
-
-    return user
+export class PrismaUsersRepository implements UsersRepository {
+  async insert({
+    name,
+    city,
+    country,
+    favorite_sport,
+  }: Prisma.UserCreateInput) {
+    try {
+      const user = await prisma.user.create({
+        data: {
+          name,
+          city,
+          country,
+          favorite_sport,
+        },
+      });
+      return user;
+    } catch (e: unknown) {
+      console.log("erro", e);
+      throw e;
+    }
   }
-  
-  async findUserByName(name: string){
-    const user = await prisma.user.findFirst({
-      where: {
-        name: name
-      }
-    })
 
-    return user || null
+  async findUserByName(name: string) {
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          name,
+        },
+      });
+      return user;
+    } catch (e: unknown) {
+      console.log("erro", e);
+      throw e;
+     }
   }
 
-  async searchMany(query: string){
-    const lowerCaseQuery = query.toLowerCase()
+  async searchMany(query: string) {
+    const lowerCaseQuery = query.toLowerCase();
 
-    if(!query) return await prisma.user.findMany();
+    if (!query) return await prisma.user.findMany();
 
-    const users = await prisma.user.findMany({
-      where: {
-        OR: [
-          { name: { contains: lowerCaseQuery } },
-          { city: { contains: lowerCaseQuery } },
-          { country: { contains: lowerCaseQuery } },
-          { favorite_sport: { contains: lowerCaseQuery } },
-        ],
-      },
-    })
-    return users;
+    try {
+      const users = await prisma.user.findMany({
+        where: {
+          OR: [
+            {
+              name: {
+                contains: lowerCaseQuery,
+              },
+            },
+            {
+              city: {
+                contains: lowerCaseQuery,
+              },
+            },
+            {
+              country: {
+                contains: lowerCaseQuery,
+              },
+            },
+            {
+              favorite_sport: {
+                contains: lowerCaseQuery,
+              },
+            },
+          ],
+        },
+      });
+      return users;
+    } catch (e: unknown) {
+      console.log("erro", e);
+      throw e;
+    }
   }
 }
